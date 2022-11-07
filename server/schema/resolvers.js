81636c969538742104c77639713b1ac37426e619
AleksaMik <aleksam.2509@gmail.com>
2022-11-07 12:24:19 -0600

    resolvers.js mutations added

--- server/schema/resolvers.js
+++ server/schema/resolvers.js
@@ -19,3 +19,41 @@ const resolvers = {
             throw new AuthenticationError('You need to be logged in!');
         },
     },
+    Mutation: {
+        addUser: async (parent, { username, email, password }) => {
+            const user = await User.create({ username, email, password });
+
+            const token = signToken(user);
+
+            return { token, user };
+        },
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+
+            if (!user) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+
+            const token = signToken(user);
+
+            return { token, user };
+        },
+        saveBook: async (parent, args, context) => {
+            if (context.user) {
+                const updatedUserBooks = await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $addToSet: { savedBooks: args } },
+                    {
+                        new: true,
+                        runValidators: true,
+                    }
+                );
+
+                return updatedUserBooks;
+            }
