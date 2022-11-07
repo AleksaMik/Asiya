73ba6b6c38594ca2cbbdd995df21e59327d1ee15
AleksaMik <aleksam.2509@gmail.com>
2022-11-07 14:19:35 -0600

    server.js updated

--- server/server.js
+++ server/server.js
@@ -36,16 +36,8 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
-const startApolloServer = async (typeDefs, resolvers) => {
-  await server.start();
-  server.applyMiddleware({ app });
 
 db.once('open', () => {
-  app.listen(PORT, () => {
-    console.log(`API server running on ${PORT}!`);
+  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
   console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-})
-})
-};
-
-startApolloServer(typeDefs, resolvers);
+});
