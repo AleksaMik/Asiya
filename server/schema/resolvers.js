322e7337d4f7ab5209266626b86e15849f531ba2
AleksaMik <aleksam.2509@gmail.com>
2022-11-07 12:14:09 -0600

    resolvers.js upd

---
+++ server/schema/resolvers.js
@@ -0,0 +1,21 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { User } = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        users: async () => {
+            return User.find({});
+        },
+        user: async (parent, args) => {
+            return User.findOne({
+                args
+            });
+        },
+        me: async (parent, args, context) => {
+            if (context.user) {
+              return User.findOne({ _id: context.user._id }).populate('savedBooks');
+            }
+            throw new AuthenticationError('You need to be logged in!');
+        },
+    },
